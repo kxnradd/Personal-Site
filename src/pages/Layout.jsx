04ff1bc38ff6c35248e02fc859282a047673c4c1
kxnradd <129627060+kxnradd@users.noreply.github.com>
2025-07-19 20:07:24 +0100

    layout.jsx: updated navbar code

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,14 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 const Layout = () => {
   return (
     <>
       <nav className="fixed top-0 mb-10 w-full bg-gray-100 dark:bg-black text-black dark:text-white">
           <div className="border-b-2 dark:border-gray-100 border-black p-2">
             <ul className="flex flex-row gap-5 justify-center">
-              <li><Link to="/">home</Link></li>
-              <li><Link to="/about">about</Link></li>
-              <li><Link to="/projects">projects</Link></li>
-              <li><Link to="/contact">contact</Link></li>
+              <li><NavLink className={({isActive}) => isActive ? "text-red-500" : "hover:text-red-500"} to="/">home</NavLink></li>
+              <li><NavLink className={({isActive}) => isActive ? "text-red-500" : "hover:text-red-500"} to="/about">about</NavLink></li>
+              <li><NavLink className={({isActive}) => isActive ? "text-red-500" : "hover:text-red-500"} to="/projects">projects</NavLink></li>
+              <li><NavLink className={({isActive}) => isActive ? "text-red-500" : "hover:text-red-500"} to="/contact">contact</NavLink></li>
             </ul>
           </div>
       </nav>
@@ -17,7 +17,7 @@ const Layout = () => {
         <Outlet />
       </main>
       <footer className="fixed bottom-0 w-full text-center text-sm bg-gray-100 dark:bg-black text-black dark:text-white p-4 border-t-2 dark:border-gray-100 border-black">
-        <p> made by <a href="https://github.com/kxnradd" className="text-red-500" target="_blank" rel="noreferrer"><strong>@kxnradd</strong></a></p>
+        <p> made by <a href="https://github.com/kxnradd" className="text-red-600" target="_blank" rel="noreferrer"><strong>@kxnradd</strong></a></p>
       </footer>
     </>
   );
