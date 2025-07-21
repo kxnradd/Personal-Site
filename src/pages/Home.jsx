b802bf059e605e89f48c7f7af391276bf6cdc3dd
kxnradd <129627060+kxnradd@users.noreply.github.com>
2025-07-21 10:39:57 +0100

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 function Home() {
   return (
-    <div className="text-center">
-      <h1 className="text-6xl font-bold mb-4">welcome</h1>
-      <p className="text-lg"></p>
+    <div className="h-screen flex items-center justify-center overflow-hidden">
+      <div className="text-center flex flex-col items-start p-4">
+        <h1 className="text-6xl font-bold mb-4 text-red-500">@kxnrad.</h1>
+        <p className="text-lg">programmer first, creative second.</p>
+        <p className="pt-4 gap-4">-<Link to="/about" className="hover:text-red-500"> more about me...</Link></p>
+        <p>-<Link to="/projects" className="hover:text-red-500"> see what I've made...</Link></p>
+        <p>-<Link to="/contact" className="hover:text-red-500"> get in touch...</Link></p>
+      </div>
     </div>
   );
 }
