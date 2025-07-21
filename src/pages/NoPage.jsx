4627270b4e845f9603368d5013c65c82de25abd3
kxnradd <129627060+kxnradd@users.noreply.github.com>
2025-07-21 10:42:54 +0100

    updated NoPage

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 function NoPage() {
   return (
     <div className="text-center align-middle">
-      <h1 className="text-6xl font-bold mb-4">page not found</h1>
+      <h1 className="text-6xl font-bold mb-4 pt-60">page not found</h1>
       <Link to="/">try again?</Link>
     </div>
   );
